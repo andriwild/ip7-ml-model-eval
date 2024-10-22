54c6c8998c868ff13fde8d8f71b18de4696ee519
Andri Wild <andri.wild@students.fhnw.ch>
2024-10-22 10:44:04 +0200

    add time measurement with decorator

--- cpu/inference.py
+++ cpu/inference.py
@@ -2,27 +2,128 @@
 # curl -LO http://images.cocodataset.org/zips/val2017.zip
 
 import torch
+from PIL import Image, ImageDraw
+from torch import nn
 from torch.utils.data import DataLoader
+from dataclasses import dataclass
+from torch.utils.data import DataLoader
+import numpy as np
+from helpers.benchmark import TimeMeasure
+from typing import List
+from time import perf_counter
 
-from benchmark import Measure
+@dataclass
+class DetectionBox:
+    x_max: float
+    y_max: float
+    x_min: float
+    y_min: float
+
+    def __repr__(self):
+        return f"Box(x_min={self.x_min}, y_min={self.y_min}, x_max={self.x_max}, y_max={self.y_max})"
+
+    def to_tuple(self):
+        # PIL conform box tuple
+        return (self.x_min, self.y_min, self.x_max, self.y_max) 
+    
+    def scale(self, scale_x, scale_y) -> 'DetectionBox':
+       return DetectionBox(
+           self.x_min * scale_x,
+           self.y_min * scale_y,
+           self.x_max * scale_x,
+           self.y_max * scale_y
+           )
+
+    def add_margin(self, margin: int, max_w, max_h) -> 'DetectionBox':
+        # add margin to the box, if the margin exceeds the image size, clip it
+        return DetectionBox(
+                np.clip(self.x_min - margin, 0, max_w),
+                np.clip(self.y_min - margin, 0, max_h),
+                np.clip(self.x_max + margin, 0, max_w),
+                np.clip(self.y_max + margin, 0, max_h),
+            )
 
 
+
+@dataclass
 class CpuInference:
 
-    def __init__(self, dataloader: DataLoader, model):
-        self.dataloader = dataloader
-        self.model = model
+    flower_model: nn.Module
+    flower_model_dim: tuple[int, int]
+    pollinator_model: nn.Module
+    pollinator_model_dim: tuple[int, int]
+
+    @TimeMeasure(name="flower_inference")
+    def _run_inference(self, images):
+        return self.flower_model(images)
+
+    @TimeMeasure(name="flower_postprocessing")
+    def _postprocessing(self, detections, image_specs) -> List[Image.Image]:
+
+        cropped_images = []
+
+        for (det, spec) in zip(detections.tolist(), image_specs):
+
+            image_original = spec.original
+            draw = ImageDraw.Draw(image_original)
+
+            # calculate detection boxes in original image coordinates
+            dataframes = det.pandas()
+            dataframe = dataframes.xyxy[0]
+            predictions = dataframe.itertuples(index=False)
+
+            for prediction in predictions:
+                w_original, h_original = spec.original.size
+
+                scale_x = w_original / (self.flower_model_dim[0] - spec.horizontal_pad)
+                scale_y = h_original / (self.flower_model_dim[1] - spec.vertical_pad)
+
+                x_max = prediction.xmax - spec.horizontal_pad / 2
+                y_max = prediction.ymax - spec.vertical_pad   / 2
+                x_min = prediction.xmin - spec.horizontal_pad / 2
+                y_min = prediction.ymin - spec.vertical_pad   / 2
+
+                box = DetectionBox(x_max, y_max, x_min, y_min)
+                box = box.scale(scale_x, scale_y) \
+                        .add_margin(10, w_original, h_original)
+
+                cropped_image = spec.original.crop(box.to_tuple())
+                cropped_images.append(cropped_image)
+
+        return cropped_images
+
+                # draw.rectangle(box.to_tuple(), width=8, outline='blue')
+                # image_original.show()
 
 
-    def warm_up(self):
-           for images, _labels in self.dataloader:
-               x = torch.stack(images)
-               out = self.model(x)
-               break  # Only one batch for warm-up
+    def predict_flowers(self, dataloader: DataLoader) -> list[Image.Image]:
+        all_cropped_images: list[Image.Image] = []
 
-    @Measure
-    def run(self):
         with torch.no_grad():
-            for images, _ in self.dataloader:
-                x = torch.stack(images)
-                _ = self.model(x)
+            for image_specs in dataloader:
+                resized_images = [spec.resized for spec in image_specs]
+                detections = self._run_inference(resized_images)
+
+                cropped_images = self._postprocessing(detections, image_specs)
+                all_cropped_images.extend(cropped_images)
+
+        return all_cropped_images
+
+
+    @TimeMeasure(name="pollinator_inference")
+    def _pollinator_inference(self, images):
+        return self.pollinator_model(images)
+
+    
+    def predict_pollinators(self, dataloader: DataLoader):
+        with torch.no_grad():
+            for images_specs in dataloader:
+                resized_images = [spec.resized for spec in images_specs]
+                detections = self._pollinator_inference(resized_images)
+                #for (det, _) in zip(detections.tolist(), images_specs):
+
+                    # calculate detection boxes in original image coordinates
+                    #for row in det.pandas().xyxy[0].itertuples(index=False):
+                        #if row.confidence > 0.8:
+                            #detections.show()
+
---
+++ run_cpu.py
@@ -0,0 +1,95 @@
+import os
+from time import sleep
+import csv
+
+import torch
+from torch import nn
+from typing import cast
+from torch.utils.data import DataLoader, Subset
+from torchvision import datasets, transforms
+from cpu.inference import CpuInference
+from helpers.arguments import parse_args
+from helpers.yolomodel import YoloModel
+
+import matplotlib.pyplot as plt
+
+from helpers.time_diagram import time_plot
+from helpers.custom_dataset import FilesystemDataset, InMemoryDateset, flower_preprocessing, pollinator_preprocessing
+
+    #transform = transforms.Compose([
+    #    transforms.Resize(640),
+    #    #transforms.ToTensor(),
+    #])
+
+    # Load COCO dataset
+    # dataset = datasets.CocoDetection(
+    #     root=args.coco_root + '/val2017',
+    #     annFile=args.coco_root + '/annotations/instances_val2017.json',
+    #     transform=transform
+    # )
+
+    # model = torch.hub.load('ultralytics/yolov5', 'yolov5n')
+    # model = YoloModel('models/flower_n.pt')
+    # model.eval()
+    # model.to('cpu')
+
+
+
+FLOWER_MODEL_PATH     = 'models/flowers_ds_v5_640_yolov5n_box_hyps_v0.pt'
+POLLINATOR_MODEL_PATH = 'models/pollinators_ds_v6_480_yolov5s_hyps_v0.pt'
+MODEL_HUB             = 'ultralytics/yolov5'
+FLOWER_MODEL_DIM      = (640, 640)
+POLLINATOR_MODEL_DIM  = (480, 480)
+
+
+def main() -> None:
+    args = parse_args()
+    n_images = args.dataset_size
+    torch.set_num_threads(args.threads)
+
+    flower_model     = cast(nn.Module, torch.hub.load(MODEL_HUB, 'custom', path=FLOWER_MODEL_PATH))
+    pollinator_model = cast(nn.Module, torch.hub.load(MODEL_HUB, 'custom', path=POLLINATOR_MODEL_PATH))
+
+    flower_preprocessing_fn = lambda img: flower_preprocessing(FLOWER_MODEL_DIM, img)
+    pollinator_preprocessing_fn = lambda img: pollinator_preprocessing(POLLINATOR_MODEL_DIM, img)
+
+
+    image_dataset = FilesystemDataset(
+        #root='/home/andri/repos/ip7-ml-model-eval/images/root',
+        root='/home/andri/minio/images/',
+        transform=flower_preprocessing_fn
+    )
+
+    # truncate dataset to the number of images specified
+    subset_range = list(range(min(n_images, len(image_dataset))))
+    subset_dataset = Subset(image_dataset, subset_range)
+
+    flower_dataloader = DataLoader(
+        dataset=subset_dataset,
+        batch_size=args.batch_size,
+        shuffle=False,
+        collate_fn=lambda x: x
+    )
+
+    inference = CpuInference(flower_model, FLOWER_MODEL_DIM, pollinator_model, POLLINATOR_MODEL_DIM)
+
+    predicted_flowers = inference.predict_flowers(flower_dataloader)
+
+    flower_dataset = InMemoryDateset(
+        images=predicted_flowers,
+        transform=pollinator_preprocessing_fn
+        )
+
+    pollinator_dataloader = DataLoader(
+         dataset=flower_dataset,
+         batch_size=args.batch_size,
+         shuffle=False,
+         collate_fn=lambda x: x
+     )
+
+    inference.predict_pollinators(pollinator_dataloader)
+
+
+
+if __name__ == "__main__":
+    main()
