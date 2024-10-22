94b09a74b75c59a6076a9241b84b708e662b3484
Andri Wild <andri.wild@students.fhnw.ch>
2024-10-22 13:19:01 +0200

    add csv writer

---
+++ helpers/CsvWriter.py
@@ -0,0 +1,26 @@
+import csv
+import os
+
+class CSVWriter:
+    def __init__(self, filename, header):
+        self.filename = filename
+        self.header = header
+
+        # Check if file exists; if not, write header
+        file_exists = os.path.isfile(self.filename)
+        if not file_exists:
+            with open(self.filename, 'w', newline='') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(self.header)
+
+    def append_data(self, data):
+        with open(self.filename, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(data)
+
+if __name__ == "__main__":
+    header = ['Function', 'Execution Time (seconds)']
+    csv_writer = CSVWriter('measurement.csv', header)
+
+    csv_writer.append_data(['function1', 0.123])
+    csv_writer.append_data(['function2', 0.456])
