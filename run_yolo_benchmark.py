695c401a88436b81be4402878d796959ec1381b9
Andri Wild <andri.wild@students.fhnw.ch>
2024-11-01 16:19:15 +0100

    remove model available check to load from ultralytics

--- run_yolo_benchmark.py
+++ run_yolo_benchmark.py
@@ -45,9 +45,6 @@ if __name__ == "__main__":
     n_images = args.n_images
     target = args.target
 
-    if not os.path.exists(args.base_model):
-        raise FileNotFoundError(f"Model file not found: {model}")
-
     with open("config.yaml", "r") as stream:
         try:
             cfg = yaml.safe_load(stream)
