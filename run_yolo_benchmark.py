069876997475c23a02c931f906c7757bbc1629f4
Andri Wild <andri.wild@students.fhnw.ch>
2024-11-01 17:23:50 +0100

    update time measurement

--- run_yolo_benchmark.py
+++ run_yolo_benchmark.py
@@ -21,11 +21,10 @@ def main(base_model, target, n_images, image_folder):
     target_model.predict(images[0], device="cpu")
 
     # Start the benchmark
-    #start_time = time.process_time()
-    start_time = time.time()
-    target_model.predict(images, device="cpu")
-    #end_time = time.process_time()
-    end_time = time.time()
+    start_time = time.process_time()
+    for image in images:
+        target_model.predict(image, device="cpu")
+    end_time = time.process_time()
 
     cprint(f"Total time: {end_time - start_time}", "green")
     cprint(f"Number of processed images: {n_images}", "green")
