124ab578d2b378de8efa4cfb2a0d9a3aeb25d910
Andri Wild <andri.wild@students.fhnw.ch>
2024-11-01 11:17:42 +0100

    update hailo benchmark script

--- benchmark_hailo.sh
+++ benchmark_hailo.sh
@@ -6,4 +6,4 @@ DEFAULT_N_IMAGES=10
 
 N_IMAGES=${1:-$DEFAULT_N_IMAGES}
 
-python3 run_hailo.py --model_path models/hailo/yolov8n.hef --batch_size 10 --dataset_size $N_IMAGES
+python3 run_hailo.py --model models/hailo/yolov8n.hef --batch_size 10 --n_images $N_IMAGES
