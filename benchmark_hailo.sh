0be8f801af2c8107b9aff5b816f44bec44d87b71
Andri Wild <andri.wild@students.fhnw.ch>
2024-11-05 16:49:20 +0100

    update hailo benchmark

--- benchmark_hailo.sh
+++ benchmark_hailo.sh
@@ -3,32 +3,27 @@
 source .hailo/bin/activate
 
 DEFAULT_N_IMAGES=10
-DEFAULT_BATCH_SIZE=10
+DEFAULT_BATCH_SIZE=1
 
 N_IMAGES=${1:-$DEFAULT_N_IMAGES}
 
-python3 run_hailo.py --model yolov10b --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES 
-sleep 2
-python3 run_hailo.py --model yolov10n --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES
-sleep 2
-python3 run_hailo.py --model yolov10s --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES
-sleep 2
-python3 run_hailo.py --model yolov10x --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES
-sleep 2
+MODELS=(
+    yolov10b
+    yolov10n
+    yolov10s
+    yolov10x
+    yolov5m
+    yolov5s
+    yolov8l
+    yolov8m
+    yolov8n
+    yolov8s
+    yolov8x
+)
 
-python3 run_hailo.py --model yolov5m  --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES
-sleep 2
-python3 run_hailo.py --model yolov5s  --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES
-
-python3 run_hailo.py --model yolov8l  --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES
-sleep 2
-python3 run_hailo.py --model yolov8m  --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES
-sleep 2
-python3 run_hailo.py --model yolov8n  --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES
-sleep 2
-python3 run_hailo.py --model yolov8s  --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES
-sleep 2
-python3 run_hailo.py --model yolov8x  --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES
-sleep 2
+for MODEL in "${MODELS[@]}"; do
+    python3 run_hailo.py --model "$MODEL" --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES
+    sleep 2 
+done
 
 deactivate
