e3eacabe7a0516e9de4c926da3e9e7d5419950f2
Andri <andri.wild@students.fhnw.ch>
2024-11-05 10:13:44 +0100

    add num images

--- utility/loader.py
+++ utility/loader.py
@@ -1,12 +1,15 @@
 import os
 
-def load_all_images(folder_path: str) -> list[str]:
-    all_images = []
+def load_all_images(folder_path: str, num_images: int) -> list[str]:
+    images = []
     img_ext: list[str] = ['.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff']
 
     for file in os.listdir(folder_path):
         if any(file.lower().endswith(ext) for ext in img_ext):
             img_path: str = os.path.join(folder_path, file)
-            all_images.append(img_path)
+            images.append(img_path)
+            if len(images) >= num_images:
+                break
 
-    return all_images
+    return images
+
