bdfa388f5ed3af7bb9697f6f046973a4d4bbb0dc
Andri Wild <andri.wild@students.fhnw.ch>
2024-11-05 16:37:55 +0100

    add sleep statements to slow down the script

--- benchmark_hailo.sh
+++ benchmark_hailo.sh
@@ -8,17 +8,27 @@ DEFAULT_BATCH_SIZE=10
 N_IMAGES=${1:-$DEFAULT_N_IMAGES}
 
 python3 run_hailo.py --model yolov10b --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES 
+sleep 2
 python3 run_hailo.py --model yolov10n --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES
+sleep 2
 python3 run_hailo.py --model yolov10s --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES
+sleep 2
 python3 run_hailo.py --model yolov10x --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES
+sleep 2
 
 python3 run_hailo.py --model yolov5m  --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES
+sleep 2
 python3 run_hailo.py --model yolov5s  --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES
 
 python3 run_hailo.py --model yolov8l  --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES
+sleep 2
 python3 run_hailo.py --model yolov8m  --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES
+sleep 2
 python3 run_hailo.py --model yolov8n  --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES
+sleep 2
 python3 run_hailo.py --model yolov8s  --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES
+sleep 2
 python3 run_hailo.py --model yolov8x  --batch_size $DEFAULT_BATCH_SIZE --n_images $N_IMAGES
+sleep 2
 
 deactivate
