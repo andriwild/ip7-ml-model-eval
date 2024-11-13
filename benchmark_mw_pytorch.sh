f8c5a837a376d858c9c00ccae237fced8f919ff8
Andri Wild <andri.wild@students.fhnw.ch>
2024-11-13 13:54:58 +0100

    fix args in mw benchmark

--- benchmark_mw_pytorch.sh
+++ benchmark_mw_pytorch.sh
@@ -6,16 +6,16 @@ DEFAULT_N_IMAGES=20
 
 N_IMAGES=${1:-$DEFAULT_N_IMAGES}
 
-python3 ./run_mw_pytorch.py --threads 1 --n_images $N_IMAGES --batch_size 1
-python3 ./run_mw_pytorch.py --threads 4 --n_images $N_IMAGES --batch_size 1
+python3 ./run_mw_pytorch.py --threads 1 --dataset_size $N_IMAGES --batch_size 1
+python3 ./run_mw_pytorch.py --threads 4 --dataset_size $N_IMAGES --batch_size 1
 
-python3 ./run_mw_pytorch.py --threads 1 --n_images $N_IMAGES --batch_size 2
-python3 ./run_mw_pytorch.py --threads 4 --n_images $N_IMAGES --batch_size 2
+python3 ./run_mw_pytorch.py --threads 1 --dataset_size $N_IMAGES --batch_size 2
+python3 ./run_mw_pytorch.py --threads 4 --dataset_size $N_IMAGES --batch_size 2
 
-python3 ./run_mw_pytorch.py --threads 1 --n_images $N_IMAGES --batch_size 4
-python3 ./run_mw_pytorch.py --threads 4 --n_images $N_IMAGES --batch_size 4
+python3 ./run_mw_pytorch.py --threads 1 --dataset_size $N_IMAGES --batch_size 4
+python3 ./run_mw_pytorch.py --threads 4 --dataset_size $N_IMAGES --batch_size 4
 
-python3 ./run_mw_pytorch.py --threads 1 --n_images $N_IMAGES --batch_size 8
-python3 ./run_mw_pytorch.py --threads 4 --n_images $N_IMAGES --batch_size 8
+python3 ./run_mw_pytorch.py --threads 1 --dataset_size $N_IMAGES --batch_size 8
+python3 ./run_mw_pytorch.py --threads 4 --dataset_size $N_IMAGES --batch_size 8
 
 deactivate
