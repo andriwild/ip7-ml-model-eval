edc731e2d0c84b191c6dc9036227a62a4d79fb80
Andri Wild <andri.wild@students.fhnw.ch>
2024-11-14 07:49:03 +0100

    add script for mitwelten tflite pipeline

---
+++ benchmark_mw_tflite.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+source .venv/bin/activate
+
+DEFAULT_N_IMAGES=20
+
+N_IMAGES=${1:-$DEFAULT_N_IMAGES}
+
+python3 ./run_mw_tflite.py --threads 1 --dataset_size $N_IMAGES --batch_size 1
+python3 ./run_mw_tflite.py --threads 4 --dataset_size $N_IMAGES --batch_size 1
+
+python3 ./run_mw_tflite.py --threads 1 --dataset_size $N_IMAGES --batch_size 2
+python3 ./run_mw_tflite.py --threads 4 --dataset_size $N_IMAGES --batch_size 2
+
+python3 ./run_mw_tflite.py --threads 1 --dataset_size $N_IMAGES --batch_size 4
+python3 ./run_mw_tflite.py --threads 4 --dataset_size $N_IMAGES --batch_size 4
+
+python3 ./run_mw_tflite.py --threads 1 --dataset_size $N_IMAGES --batch_size 8
+python3 ./run_mw_tflite.py --threads 4 --dataset_size $N_IMAGES --batch_size 8
+
+deactivate
